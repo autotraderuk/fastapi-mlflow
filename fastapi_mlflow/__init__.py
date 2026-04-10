b191178bd1e7f863b342455c043351685d4a2ddb
John Harrison <john.harrison@autotrader.co.uk>
2026-04-10 16:05:42 +0100

    Derive `fastapi_mlflow.__version__` from package
    Derive `fastapi_mlflow.__version__` from package metadata using
    `importlib.metadata`. This will ensure that it stays up to date with the
    actual distribution version (which it hasn't been for a while!)

--- fastapi_mlflow/__init__.py
+++ fastapi_mlflow/__init__.py
@@ -1,1 +1,8 @@
-__version__ = "0.1.2"
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("fastapi-mlflow")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
+
+__all__ = ["__version__"]
